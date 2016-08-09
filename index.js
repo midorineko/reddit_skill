aaa06fe54f6195a79be8b67097514f86eb1d0104
Steven Inouye <Steven Inouye>
2016-08-09 15:48:59 -0700

    handling empty inputs

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ var speech_out = require('./speech_out');
 var sessionRequests = [];
 
 exports.handler = function( event, context ) {
-    var final = false;
-    var category = "";
     sessionRequests.push(event);
-    var current = event.request.intent.slots.Reddit.value.toLowerCase();
+    var current = 'help'
+    if ( event.request.intent ) {
+        current = event.request.intent.slots.Reddit.value.toLowerCase();
+    }
     var speechTxt = "Was that? "
-
-    speech_return = speech_out.speech(current);
-
-    final = speech_return[0];
-    category = speech_return[1];
+    var speech_return = speech_out.speech(current);
+    var final = speech_return[0];
+    var category = speech_return[1];
     current = speech_return[2];
-
     speechTxt += current;
 
     if(final){
         event = sessionRequests[sessionRequests.length - 2];
         getReddit(event, context, category);
+    }else if(current==="help"){
+        var helpTxt = 'Hello, this skill was made to help you navigate reddit. First state a sub, alexa will respond with what she heard. If it is an incorrect sub, restate the desired sub. If it is the correct sub, you may proceed by saying; yes, rising, recent, hot, top, gilded, or controversial. Alexa will then rattle off the top five stories. Please state a sub.'
+        outout.output(helpTxt, context, false);
     }else{
         outout.output(speechTxt, context, false);
     };
@@ -32,7 +33,7 @@ function getReddit(event, context, category){
     var url = null;
     var https = require( 'https' );
     url = "https://www.reddit.com/r/" + reddit_url +"/" + category + ".json?limit=5";
-    https.get( url, function( response ) {
+    var request = https.get( url, function( response ) {
         var data = '';
         response.on( 'data', function( x ) { data += x; } );
         response.on( 'end', function() {
@@ -49,4 +50,7 @@ function getReddit(event, context, category){
         } );
         
     } );
+    request.setTimeout( 10000, function( ) {
+        outout.output('sorry there was an issue with this skill', context, true);
+    });
 };
